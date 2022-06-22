e89955645c93b74eade908041b65da110102841c
Fernando Javier Aguilar Canto <pherjev@gmail.com>
2022-06-22 18:18:06 -0500

    Delete adam.py

--- adam.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import Input
-from tensorflow.keras.utils import to_categorical
-
-len_v = 
